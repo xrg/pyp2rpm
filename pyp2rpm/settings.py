b1c502f540e31cb5e9f84e2ecd6cb1638d264b6a
P. Christeas <xrg@linux.gr>
2018-05-12 12:46:25 +0300

    Mageia git-rpm template
    For RPM-based builds.

--- pyp2rpm/settings.py
+++ pyp2rpm/settings.py
@@ -6,7 +6,8 @@ DEFAULT_PYTHON_VERSIONS = {
     'epel7': ['2', '3'],
     'epel6': ['2'],
     'mageia': ['3'],
-    'pld': ['2', '3']
+    'pld': ['2', '3'],
+    'mga-git': ['2'],   # TODO: Python3 support
 }
 DEFAULT_PYTHON_VERSION = DEFAULT_PYTHON_VERSIONS[DEFAULT_TEMPLATE][0]
 DEFAULT_PKG_SOURCE = 'pypi'
